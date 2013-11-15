71ea5378c20c647441e9bed05003f5e1466fb443
srcnaks <srcnaks@gmail.com>
2013-11-15 13:33:12 +0200

    en son ne yaptıysam

--- ShoutApp/src/com/shoutapp/AddPostActivity.java
+++ ShoutApp/src/com/shoutapp/AddPostActivity.java
@@ -1,12 +1,19 @@
 package com.shoutapp;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TimePicker;
 
 public class AddPostActivity extends BaseActivity {
 
+	EditText saat;
+	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		RelativeLayout mainLayout = (RelativeLayout)findViewById(R.id.mainLayout);
@@ -20,6 +27,30 @@ public class AddPostActivity extends BaseActivity {
 		lp.addRule(RelativeLayout.BELOW,R.id.topBar);
 		addPostLayout.setLayoutParams(lp);
         mainLayout.addView(add_post);
+        saat = (EditText)findViewById(R.id.time);
+        saat.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Dialog d = (Dialog)(new TimePickerDialog(getApplicationContext(), timePickerListener, 12, 10,false));
+				d.show();
+			}
+		});
+//        TimePickerDialog
 
 	}
+	
+	private TimePickerDialog.OnTimeSetListener timePickerListener =  new TimePickerDialog.OnTimeSetListener() {
+		@Override
+		public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(selectedHour);
+			sb.append(':');
+			sb.append(selectedMinute);
+			saat.setText(sb);
+
+		}
+
+	};
+
 }
