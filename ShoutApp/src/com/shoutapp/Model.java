d07101a48121d1ba866158e2ec13b05ac2e9a1a7
utkucan <utkucanyucel@gmail.com>
2013-11-30 01:19:44 +0200

    yazılar

--- ShoutApp/src/com/shoutapp/Model.java
+++ ShoutApp/src/com/shoutapp/Model.java
@@ -8,33 +8,33 @@ public class Model {
 	
 	public static ArrayList<BadgeObject> getBadge(){
 		ArrayList<BadgeObject> badges = new ArrayList<BadgeObject>();
-		badges.add(new BadgeObject(R.drawable.events_cat_bicycle, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_cat_camp, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_cat_food, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_cat_game, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_cat_movie, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_cat_night, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_cat_uknowwhatimean, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_cat_wood, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_count_1, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_count_10, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_count_25, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_count_50, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_count_100, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_count_250, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_count_500, "23.11.2013", "hobarey"));
-		badges.add(new BadgeObject(R.drawable.events_count_king, "23.11.2013", "hobarey"));
+		badges.add(new BadgeObject(R.drawable.events_cat_bicycle, "01.01.2013", "Go Healthy"));
+		badges.add(new BadgeObject(R.drawable.events_cat_camp, "23.05.2013", "Camper"));
+		badges.add(new BadgeObject(R.drawable.events_cat_food, "09.12.2013", "Chronical Hungry"));
+		badges.add(new BadgeObject(R.drawable.events_cat_game, "13.04.2013", "Game Addict"));
+		badges.add(new BadgeObject(R.drawable.events_cat_movie, "20.01.2013", "Dr. Movie"));
+		badges.add(new BadgeObject(R.drawable.events_cat_night, "17.11.2013", "Night watcher"));
+		badges.add(new BadgeObject(R.drawable.events_cat_uknowwhatimean, "23.11.2013", "Socialize"));
+		badges.add(new BadgeObject(R.drawable.events_cat_wood, "12.05.2013", "Green Activist"));
+		badges.add(new BadgeObject(R.drawable.events_count_1, "12.11.2012", "First time"));
+		badges.add(new BadgeObject(R.drawable.events_count_10, "23.01.2013", "Tenth first time"));
+		badges.add(new BadgeObject(R.drawable.events_count_25, "11.03.2013", "25 Combo"));
+		badges.add(new BadgeObject(R.drawable.events_count_50, "05.04.2013", "Half of hundred"));
+		badges.add(new BadgeObject(R.drawable.events_count_100, "23.06.2012", "SHOUT!"));
+		badges.add(new BadgeObject(R.drawable.events_count_250, "19.08.2013", "Leader"));
+		badges.add(new BadgeObject(R.drawable.events_count_500, "05.10.2013", "Thats too much"));
+		badges.add(new BadgeObject(R.drawable.events_count_king, "01.12.2013", "Celebrity"));
 		return badges;
 	}
 	
 	public static ArrayList<CommentItemObjet> getComments(){
 		ArrayList<CommentItemObjet> comment_list = new ArrayList<CommentItemObjet>();
-        comment_list.add(new CommentItemObjet("Az sabredersen,  menemen ve dünden kalma biraz makarna getirebilirim, evde ekmek var mý? :))","Kate Austen","15:03"));
-        comment_list.add(new CommentItemObjet("asdasdas Az sabredersen,  menemen ve dünden kalma biraz makarna getirebilirim, evde ekmek var mý? :))","Kate Austen","15:03"));
-        comment_list.add(new CommentItemObjet("Az sabredersen,  menemen ve dünden kalma biraz makarna getirebilirim, evde ekmek var mý? :))","Kate Austen","15:03"));
-        comment_list.add(new CommentItemObjet("Az sabredersen,  menemen ve dünden kalma biraz makarna getirebilirim, evde ekmek var mý? :))","Kate Austen","15:03"));
-        comment_list.add(new CommentItemObjet("Az sabredersen,  menemen ve dünden kalma biraz makarna getirebilirim, evde ekmek var mý? :))","Kate Austen","15:03"));
-        comment_list.add(new CommentItemObjet("Az sabredersen,  menemen ve dünden kalma biraz makarna getirebilirim, evde ekmek var mý? :))","Kate Austen","15:03"));
+        comment_list.add(new CommentItemObjet("Az sabredersen, mercimek köftesi, ýslak kek ve dünden kalma biraz su böreði getirebilirim, evde çay var mý? :))","Kate Austen","15:03"));
+        comment_list.add(new CommentItemObjet("Gün var orada sanýrým? :))","Ayþe Teyze","15:05"));
+        comment_list.add(new CommentItemObjet("Günümüze herkes davetlidir? :))","Kate Austen","15:07"));
+        comment_list.add(new CommentItemObjet("Acayip caným istedi? :))","Mr. Crowley","15:08"));
+        comment_list.add(new CommentItemObjet("Terliklerimi çantama koydum 5 dakikaya oradayým.? :))","Leyla M.","15:12"));
+        comment_list.add(new CommentItemObjet("Marketten meyve suyu alabilir misiniz?","Kate Austen","15:13"));
         return comment_list;
 	}
 	
