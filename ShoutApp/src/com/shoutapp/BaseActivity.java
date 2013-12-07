ae60549d05e2e61c45a8b998fe889ed62e8f9ba8
utkucan <utkucanyucel@gmail.com>
2013-12-07 02:11:45 +0200

    sharedpreferences and notificationbar item

--- ShoutApp/src/com/shoutapp/BaseActivity.java
+++ ShoutApp/src/com/shoutapp/BaseActivity.java
@@ -12,87 +12,73 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class BaseActivity extends SlidingMenuBaseActivity{
-//	public PlusClient mPlusClient;
+	//	public PlusClient mPlusClient;
 	private ConnectionResult mConnectionResult;
 	private ProgressDialog mConnectionProgressDialog;
 	private Activity base_activity;
 	private static final int REQUEST_CODE_RESOLVE_ERR = 9000;
-	
-	
-	 public static final String FILTER_PREFS = "filterPrefs";
-//	OnGoogleConnectionStateChangeEventListener gListener;
+	public static final String FILTER_PREFS = "filterPrefs";
+
+	//	OnGoogleConnectionStateChangeEventListener gListener;
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		base_activity = this;
 		setSlidingActionBarEnabled(true);
 		setContentView(R.layout.main_frame);
-		
+
 		RelativeLayout slidingMenuBtnHolder = (RelativeLayout)findViewById(R.id.slidingMenuButtonHolder);
 		slidingMenuBtnHolder.setOnClickListener(slidingMenuClickListener);
 		ImageButton slidingMenuBtn = (ImageButton)findViewById(R.id.slidingMenuButton);
 		slidingMenuBtn.setOnClickListener(slidingMenuClickListener);
-		
+
 		RelativeLayout filterButtonHolder = (RelativeLayout)findViewById(R.id.filterButtonHolder);
-		
-		// filtre butonuna basýlýnca bu kod calýþacak inþallah
+
+		//filtre butonuna basýlýnca bu kod calýþacak inþallah
+
 		filterButtonHolder.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-//				Intent i = new Intent();
-//		        i.setClassName("com.shoutapp", "com.shoutapp.SeekbarActivity");
-//		        startActivity(i);
-				
-//				RelativeLayout mainLayout = (RelativeLayout)findViewById(R.id.mainLayout);
-//		        View seek = LayoutInflater.from(getBaseContext()).inflate(R.layout.activity_seekbar, null);
-//		        RelativeLayout seekLayout = (RelativeLayout)seek.findViewById(R.id.seek_layout);
-//				RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-//													      RelativeLayout.LayoutParams.FILL_PARENT,
-//													      RelativeLayout.LayoutParams.FILL_PARENT);
-////		        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
-//		        seekLayout.setLayoutParams(lp);
-//		        mainLayout.addView(seek);
-				
-				
-		        
+				readPrefs(v);
 				RelativeLayout mainLayout = (RelativeLayout)findViewById(R.id.mainLayout);
 				View seek = LayoutInflater.from(getBaseContext()).inflate(R.layout.activity_seekbar, null);
-				SeekbarActivity sa = new SeekbarActivity( seek);
-		        RelativeLayout seekLayout = (RelativeLayout)seek.findViewById(R.id.seek_layout);
+				SeekbarActivity sa = new SeekbarActivity(seek);
+				RelativeLayout seekLayout = (RelativeLayout)seek.findViewById(R.id.seek_layout);
+
 				RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-													      RelativeLayout.LayoutParams.FILL_PARENT,
-													      RelativeLayout.LayoutParams.FILL_PARENT);
-//		        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
-		        seekLayout.setLayoutParams(lp);
-		        mainLayout.addView(seek);
-		        
-		        Button filterDoneBtn = (Button) findViewById(R.id.filter_save_btn);
-				filterDoneBtn.setOnClickListener(new OnClickListener() {
-					
+						RelativeLayout.LayoutParams.FILL_PARENT,
+						RelativeLayout.LayoutParams.FILL_PARENT);
+				seekLayout.setLayoutParams(lp);
+				mainLayout.addView(seek);
+
+				Button filterDoneBtn = (Button) findViewById(R.id.filter_save_btn);
+				filterDoneBtn.setOnClickListener(new OnClickListener() {					
 					@Override
 					public void onClick(View v) {
-						// TODO Auto-generated method stub
-						 
+						//TODO Auto-generated method stub						 
 						savePrefs();						
 						RelativeLayout mainLayout = (RelativeLayout)findViewById(R.id.mainLayout);
 						View seek = (View) findViewById(R.id.seek_layout);
 						mainLayout.removeView(seek);
-						
+
 					}
 				});
 			}
 		});
-		
+
 		ImageView logo = (ImageView)findViewById(R.id.titleLogo);
 		logo.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
@@ -104,89 +90,104 @@ public class BaseActivity extends SlidingMenuBaseActivity{
 			}
 		});
 	}
-	
+	public void readPrefs(View v){
+		SharedPreferences settings = getSharedPreferences(FILTER_PREFS, 0);
+		int silent = settings.getInt("distance",-1);
+		Toast.makeText(v.getContext(), "Shouts within "+ silent +"kms will be shown!",Toast.LENGTH_SHORT).show();
+	}
+
 	public void savePrefs(){
 		//save all variables
 		SharedPreferences settings = getSharedPreferences(FILTER_PREFS, 0);
 		SharedPreferences.Editor editor = settings.edit();
-		editor.putBoolean("distance", mSilentMode);
+		editor.putInt("distance", ((SeekBar) findViewById(R.id.distance_bar)).getProgress());
+		editor.putInt("timeMin", ((SeekBar) findViewById(R.id.clock_bar)).getProgress());
+		//editor.putInt("timeMax", ((SeekBar) findViewById(R.id.distance_bar)).getProgress());
+
+		editor.putBoolean("sports", ((CheckBox) findViewById(R.id.sportCheckBox)).isChecked());
+		editor.putBoolean("party", ((CheckBox) findViewById(R.id.partyCheckBox)).isChecked());
+		editor.putBoolean("game", ((CheckBox) findViewById(R.id.gameCheckBox)).isChecked());
+		editor.putBoolean("activity", ((CheckBox) findViewById(R.id.activityCheckBox)).isChecked());
+		editor.putBoolean("art", ((CheckBox) findViewById(R.id.artCheckBox)).isChecked());
+		editor.putBoolean("other", ((CheckBox) findViewById(R.id.otherCheckBox)).isChecked());
+
 		editor.commit();
-		
 	}
-	
+
 	OnClickListener slidingMenuClickListener = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
+			readPrefs(v);
 			SlidingMenu sm = getSlidingMenu();
 			sm.toggle(true);
 		}
 	};
-	
-//	private ConnectionCallbacks gp_connectionCallback = new ConnectionCallbacks() {
-//		
-//		@Override
-//		public void onDisconnected() {
-//			Intent intent = new Intent(getBaseContext(), LoginActivity.class);
-//			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Removes other Activities from stack
-//			startActivity(intent);
-//			gListener.onDisconnect();
-//		}
-//		
-//		private Bitmap getImageBitmap(String url) {
-//	        Bitmap bm = null;
-//	        try {
-//	            URL aURL = new URL(url);
-//	            URLConnection conn = aURL.openConnection();
-//	            conn.connect();
-//	            InputStream is = conn.getInputStream();
-//	            BufferedInputStream bis = new BufferedInputStream(is);
-//	            bm = BitmapFactory.decodeStream(bis);
-//	            bis.close();
-//	            is.close();
-//	       } catch (IOException e) {
-////	           Log.e(TAG, "Error getting bitmap", e);
-//	       }
-//	       return bm;
-//	    }
-		
-//		@Override
-//		public void onConnected(Bundle connectionHint) {
-//			Person user = mPlusClient.getCurrentPerson();
-//			if(user != null){
-//				Intent intent = new Intent(getBaseContext(), MainActivity.class);
-////				intent.putExtra("loginType", "g");
-//				startActivity(intent);
-//				gListener.onConnect();
-//			}else{
-//				mPlusClient.disconnect();
-//			}
-//		}
-//	};
-//	private OnConnectionFailedListener gp_OnConnectionFailedListener = new OnConnectionFailedListener() {
-//		
-//		@Override
-//		public void onConnectionFailed(ConnectionResult result) {
-//			if (mConnectionProgressDialog.isShowing()) {
-//	               if (result.hasResolution()) {
-//	                       try {
-//	                               result.startResolutionForResult(base_activity, REQUEST_CODE_RESOLVE_ERR);
-//	                       } catch (SendIntentException e) {
-//	                               mPlusClient.connect();
-//	                       }
-//	               }
-//	       }
-//			mConnectionResult = result;
-//		}
-//	};
-//	
-//	public void setOnGoogleConnectionStateChangeEvent(OnGoogleConnectionStateChangeEventListener listener){
-//		gListener = listener;
-//	}
-//	
-//	public interface OnGoogleConnectionStateChangeEventListener{
-//		public void onConnect();
-//		public void onDisconnect();
-//	}
+
+	//	private ConnectionCallbacks gp_connectionCallback = new ConnectionCallbacks() {
+	//		
+	//		@Override
+	//		public void onDisconnected() {
+	//			Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+	//			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Removes other Activities from stack
+	//			startActivity(intent);
+	//			gListener.onDisconnect();
+	//		}
+	//		
+	//		private Bitmap getImageBitmap(String url) {
+	//	        Bitmap bm = null;
+	//	        try {
+	//	            URL aURL = new URL(url);
+	//	            URLConnection conn = aURL.openConnection();
+	//	            conn.connect();
+	//	            InputStream is = conn.getInputStream();
+	//	            BufferedInputStream bis = new BufferedInputStream(is);
+	//	            bm = BitmapFactory.decodeStream(bis);
+	//	            bis.close();
+	//	            is.close();
+	//	       } catch (IOException e) {
+	////	           Log.e(TAG, "Error getting bitmap", e);
+	//	       }
+	//	       return bm;
+	//	    }
+
+	//		@Override
+	//		public void onConnected(Bundle connectionHint) {
+	//			Person user = mPlusClient.getCurrentPerson();
+	//			if(user != null){
+	//				Intent intent = new Intent(getBaseContext(), MainActivity.class);
+	////				intent.putExtra("loginType", "g");
+	//				startActivity(intent);
+	//				gListener.onConnect();
+	//			}else{
+	//				mPlusClient.disconnect();
+	//			}
+	//		}
+	//	};
+	//	private OnConnectionFailedListener gp_OnConnectionFailedListener = new OnConnectionFailedListener() {
+	//		
+	//		@Override
+	//		public void onConnectionFailed(ConnectionResult result) {
+	//			if (mConnectionProgressDialog.isShowing()) {
+	//	               if (result.hasResolution()) {
+	//	                       try {
+	//	                               result.startResolutionForResult(base_activity, REQUEST_CODE_RESOLVE_ERR);
+	//	                       } catch (SendIntentException e) {
+	//	                               mPlusClient.connect();
+	//	                       }
+	//	               }
+	//	       }
+	//			mConnectionResult = result;
+	//		}
+	//	};
+	//	
+	//	public void setOnGoogleConnectionStateChangeEvent(OnGoogleConnectionStateChangeEventListener listener){
+	//		gListener = listener;
+	//	}
+	//	
+	//	public interface OnGoogleConnectionStateChangeEventListener{
+	//		public void onConnect();
+	//		public void onDisconnect();
+	//	}
 }
--- ShoutApp/src/com/shoutapp/SeekbarActivity.java
+++ ShoutApp/src/com/shoutapp/SeekbarActivity.java
@@ -1,6 +1,13 @@
 package com.shoutapp;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -8,32 +15,42 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 public class SeekbarActivity extends Activity{
 
 	private SeekBar distanceControl,timeControl = null;
 	private TextView distanceLabel , timeLabel = null;
 	private Button filterDoneBtn = null;
+
+	 
 	View parent2;
 	public SeekbarActivity(View parent){
 		this.parent2 = parent;
-
-
+		newNotification(parent.getContext());
+		
 		distanceControl = (SeekBar) parent.findViewById(R.id.distance_bar);
+		int distance = getDistance(parent.getContext());
+		distanceControl.setProgress(distance);
+		
 		//volumeControl.setThumb(writeOnDrawable(R.drawable.add_event_button, volumeControl.getProgress()+""));
 		distanceLabel = ((TextView) parent.findViewById(R.id.distanceLabel));
 		distanceLabel.setText(distanceControl.getProgress()+"");
-
+		
 		distanceControl.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 			int progressChanged = 0;
 
@@ -68,9 +85,10 @@ public class SeekbarActivity extends Activity{
 
 
 		timeControl = (SeekBar)  parent.findViewById(R.id.clock_bar);
+		timeControl.setProgress(getTime(parent.getContext()));
 		timeLabel = ((TextView) parent.findViewById(R.id.timeLabel));
 		timeLabel.setText(timeControl.getProgress()+"");
-
+		
 
 		timeControl.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 			int progressChanged = 0;
@@ -99,8 +117,17 @@ public class SeekbarActivity extends Activity{
 				//timeControl.setThumb(writeOnDrawable(R.drawable.add_event_button, progressChanged+""));
 			}
 		});
+		
+		boolean[] checkList = getCheckBoxes(parent.getContext());
+		
 
-
+		((CheckBox) parent.findViewById(R.id.sportCheckBox)).setChecked(checkList[0]);
+		((CheckBox) parent.findViewById(R.id.partyCheckBox)).setChecked(checkList[1]);
+		((CheckBox) parent.findViewById(R.id.gameCheckBox)).setChecked(checkList[2]);
+		((CheckBox) parent.findViewById(R.id.activityCheckBox)).setChecked(checkList[3]);
+		((CheckBox) parent.findViewById(R.id.artCheckBox)).setChecked(checkList[4]);
+		((CheckBox) parent.findViewById(R.id.otherCheckBox)).setChecked(checkList[5]);
+		
 	}
 	public BitmapDrawable writeOnDrawable(int drawableId, String text){
 
@@ -116,13 +143,58 @@ public class SeekbarActivity extends Activity{
 
 		return new BitmapDrawable(bm);
 	}
+	public void newNotification(Context ctx){
+		NotificationCompat.Builder mBuilder =
+		        new NotificationCompat.Builder(ctx)
+		        .setSmallIcon(R.drawable.icon)
+		        .setContentTitle("Yeni etkinlik!")
+		        .setContentText("100 metre uzaðýnýzda tam size göre bir etkinlik var!");
+		// Creates an explicit intent for an Activity in your app
+		Intent resultIntent = new Intent(ctx, BaseActivity.class);
 
-
-
-
+		// The stack builder object will contain an artificial back stack for the
+		// started Activity.
+		// This ensures that navigating backward from the Activity leads out of
+		// your application to the Home screen.
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(ctx);
+		// Adds the back stack for the Intent (but not the Intent itself)
+		stackBuilder.addParentStack(MainActivity.class);
+		// Adds the Intent that starts the Activity to the top of the stack
+		stackBuilder.addNextIntent(resultIntent);
+		PendingIntent resultPendingIntent =
+		        stackBuilder.getPendingIntent(
+		            0,
+		            PendingIntent.FLAG_UPDATE_CURRENT
+		        );
+		mBuilder.setContentIntent(resultPendingIntent);
+		NotificationManager mNotificationManager =
+		    (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+		// mId allows you to update the notification later on.
+		mNotificationManager.notify(99, mBuilder.build());
+		
+	}
+	public int getDistance(Context ctx){
+		SharedPreferences settings = ctx.getSharedPreferences(BaseActivity.FILTER_PREFS, 0);
+		int silent = settings.getInt("distance",100);
+		return silent;
+	}
+	public int getTime(Context ctx){
+		SharedPreferences settings = ctx.getSharedPreferences(BaseActivity.FILTER_PREFS, 1);
+		int silent = settings.getInt("timeMin",24);
+		return silent;
+	}
+	public boolean[] getCheckBoxes(Context ctx){
+		SharedPreferences storedSettings = ctx.getSharedPreferences(BaseActivity.FILTER_PREFS, 0);
+	return new boolean[]{
+			storedSettings.getBoolean("sports",true),
+			storedSettings.getBoolean("party",true),
+			storedSettings.getBoolean("game",true),
+			storedSettings.getBoolean("activity",true),
+			storedSettings.getBoolean("art",true),
+			storedSettings.getBoolean("other",true)
+			};
 }
-
-
+}
 
 // 
 //public class SeekbarActivity extends Activity {
