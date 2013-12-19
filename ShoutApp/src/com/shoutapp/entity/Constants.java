e5e2c1647306fba53142dd424f62ad0ddedfd22d
Mustafa Akin <mustafa91@gmail.com>
2013-12-19 16:24:09 +0200

    URL changed from Local to Heroku

--- ShoutApp/src/com/shoutapp/entity/Constants.java
+++ ShoutApp/src/com/shoutapp/entity/Constants.java
@@ -1,5 +1,8 @@
 package com.shoutapp.entity;
 
 public class Constants {
-	final public static String URL = "http://192.168.1.105:5000/";
+	final public static String LOCAL_URL = "http://192.168.1.105:5000/";
+	final public static String HEROKU_URL = "http://shoutaround.herokuapp.com/";
+	final public static String URL = HEROKU_URL;
+
 }
